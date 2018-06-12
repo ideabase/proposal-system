92be172712bdbe2bf02863b9529700233e8a2098
Theodore Byron <ted@tedbyron.com>
2018-06-12 17:25:24 -0400

    Update proposal entry with print button, footer

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,7 @@
+function printDiv(id) {
+     var printContents = document.getElementById(id).innerHTML;
+     var originalContents = document.body.innerHTML;
+     document.body.innerHTML = printContents;
+     window.print();
+     document.body.innerHTML = originalContents;
+}
